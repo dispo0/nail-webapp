1416da56bb27bcbbf42b387aa9743c9023a9fdb3
dispo0 <130572984+dispo0@users.noreply.github.com>
2025-06-26 03:14:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-window.Telegram.WebApp.ready(); // инициализация WebApp
+window.Telegram.WebApp.ready();
 
-const tgUser = window.Telegram.WebApp.initDataUnsafe.user;
+const tgUser = Telegram.WebApp.initDataUnsafe.user;
+document.getElementById("username").innerText = tgUser?.first_name || "Guest";
 
-if (tgUser) {
-  document.getElementById("username").innerText = tgUser.first_name;
-} else {
-  document.getElementById("username").innerText = "Guest";
-}
+// 💅 Фейковые мастера
+const masters = [
+  {
+    name: "Катя Нейлс",
+    photo: "https://i.imgur.com/4ZQZ4Vz.jpg",
+    services: ["Маникюр", "Покрытие гель-лак", "Дизайн ногтей"],
+    telegramUsername: "katya_nails"
+  },
+  {
+    name: "Анна Профи",
+    photo: "https://i.imgur.com/B0zDjNA.jpg",
+    services: ["Педикюр", "Снятие", "Укрепление"],
+    telegramUsername: "anna_profi"
+  }
+];
 
-function bookService() {
-  alert("Booking sent (not really yet 😂)");
-}
+const container = document.getElementById("masters");
+
+masters.forEach(master => {
+  const div = document.createElement("div");
+  div.className = "master";
+  div.innerHTML = `
+    <img src="${master.photo}" alt="${master.name}" />
+    <h2>${master.name}</h2>
+    <div class="services">${master.services.join(" • ")}</div>
+    <a class="book-btn" href="https://t.me/${master.telegramUsername}" target="_blank">Написать в Telegram</a>
+  `;
+  container.appendChild(div);
+});
